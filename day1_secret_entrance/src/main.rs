f63a68f073e89265af8e392dacf542e7ae1f9039
Yuki.N <Syukkic@gmail.com>
2025-12-22 16:54:41 +0800

    day1, done~

--- day1_secret_entrance/src/main.rs
+++ day1_secret_entrance/src/main.rs
@@ -2,38 +2,55 @@ use std::fs::File;
 use std::io::{self, BufRead};
 
 fn main() {
-    let mut current_point = 50;
-    let mut zero_count = 0;
+    let current_point = 50;
     let file = File::open("input.txt").expect("No input file");
     let reader = io::BufReader::new(file);
     let rotation_seq: Vec<String> = reader
         .lines()
         .map(|line| line.expect("Failed to read line"))
         .collect();
+    let zero_count = calc_password(current_point, rotation_seq);
+
+    println!("password: {}", zero_count);
+}
+
+fn calc_password(mut current_point: i32, rotation_seq: Vec<String>) -> i32 {
+    let mut zero_count = 0;
 
     rotation_seq.iter().for_each(|r| {
         let (direction, num_str) = r.split_at(1);
-        let number = num_str.parse().ok().unwrap_or(0);
+        let steps = num_str.parse().ok().unwrap_or(0);
 
         match direction {
             "L" => {
-                current_point -= number;
-                while current_point < 0 {
-                    current_point += 100
+                let first_zero = if current_point % 100 == 0 {
+                    100
+                } else {
+                    current_point % 100
+                };
+                if first_zero <= steps {
+                    zero_count += 1 + (steps - first_zero) / 100;
                 }
+
+                current_point = (current_point - steps).rem_euclid(100)
             }
             "R" => {
-                current_point += number;
-                while current_point >= 100 {
-                    current_point -= 100
+                let steps_to_first_zero = (100 - current_point) % 100;
+                let first_zero = if steps_to_first_zero == 0 {
+                    100
+                } else {
+                    steps_to_first_zero
+                };
+
+                if first_zero <= steps {
+                    zero_count += 1 + (steps - first_zero) / 100;
                 }
+
+                current_point = (current_point + steps).rem_euclid(100)
             }
-            _ => panic!(),
-        }
-        if current_point == 0 {
-            zero_count += 1
+            _ => unreachable!(),
         }
     });
 
-    println!("password: {}", zero_count);
+    zero_count
 }
